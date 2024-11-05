714ad862d4178b7df1e5a8c49a2885dd3d9245a6
seokyHan <sseok1979@naver.com>
2024-11-05 14:48:47 +0900

    fix : Business Layer 테스트(주문번호를 통한 주문생성)

---
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -0,0 +1,42 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.Order;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+    public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
+        List<String> productNumbers = request.getProductNumbers();
+        List<Product> products = findProductBy(productNumbers);
+
+        Order order = Order.create(products, registeredDateTime);
+        Order saveOrder = orderRepository.save(order);
+
+        return OrderResponse.of(saveOrder);
+    }
+
+    private List<Product> findProductBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        return productNumbers.stream()
+                .map(productNumber -> productMap.get(productNumber))
+                .collect(Collectors.toList());
+    }
+}
