1542341c7175094a41ddf283e3517989203f1f38
seokyHan <sseok1979@naver.com>
2024-11-05 17:36:24 +0900

    fix : 상품 조회 테스트 추가

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -20,6 +20,13 @@ import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 @DataJpaTest
 class ProductRepositoryTest {
 
+    /**
+     * OrderServiceTest에서는 데이터 클랜징 작업을 해줘야 통합테스트가 통과하지만
+     * repository test에서 그냥 통과하는 이유는
+     * @DataJpaTest 때문(해당 어노테이션 안에 @Transactional이 존재하기 때문에 테스트 후 자동 롤백)
+     * @SpringBootTest에는 @Transactional 없음
+     */
+
     @Autowired
     private ProductRepository productRepository;
 
@@ -53,6 +60,45 @@ class ProductRepositoryTest {
 
         // when
         List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+        //then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus") // 쓰고자 하는 필드만 추출
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                ); // 순서 상관없이 맞는지 체크
+    }
+
+    @DisplayName("상품번호 리스트로 상품 조회")
+    @Test
+    void findAllByProductNumberInTest() {
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001", "002"));
 
         //then
         assertThat(products).hasSize(2)
