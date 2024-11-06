d490f17a9cb6d24f4a86299a63781696b626a17f
seokyHan <sseok1979@naver.com>
2024-11-06 10:29:16 +0900

    fix: 주석 문구 수정

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -3,7 +3,6 @@ package sample.cafekiosk.spring.api.service.product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 import sample.cafekiosk.spring.api.controller.product.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -13,14 +12,12 @@ import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
-import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
 /**
  * @Transactional(readOnly = true)
  * readOnly = true : 읽기전용
  * CRUD 에서 CUD 동작 X / only READ
- * JPA : CUD 스냅샷 저장, 변경감지 X (성느으 향상)
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
  *
  * CQRS - Command / Read
  */
