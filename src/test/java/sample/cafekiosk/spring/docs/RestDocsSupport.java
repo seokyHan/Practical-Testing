94069d70f29096cf18b751c044e70ab5d0e63c49
seokyHan <sseok1979@naver.com>
2024-11-11 10:47:24 +0900

    add: Spring REST Docs 실습

---
+++ src/test/java/sample/cafekiosk/spring/docs/RestDocsSupport.java
@@ -0,0 +1,31 @@
+package sample.cafekiosk.spring.docs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class RestDocsSupport {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper mapper = new ObjectMapper();
+
+    // MockMvcBuilders.webAppContextSetup(new WebApplicationContext()) 의 경우 Spring boot test 서버가 실제 올라가서
+    // 테스트가 진행되기 때문에 서버 올라가는 것과 관계 없이(spring 의존 없이) Controller에 대한 단위테스트로 실행
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
+                .apply(documentationConfiguration(provider))
+                .build();
+    }
+
+    protected abstract Object initController();
+}
