59f1b232666d7b26f3084191ab23f118b01d806a
seokyHan <sseok1979@naver.com>
2024-11-11 10:46:42 +0900

    fix: 결제 완료 주문목록 조회 테스트 리팩토링

--- src/test/java/sample/cafekiosk/spring/domain/order/OrderRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/order/OrderRepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.IntegrationTestSupport;
 import sample.cafekiosk.spring.domain.orderProduct.OrderProduct;
 import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductType;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -18,7 +20,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
 import static org.junit.jupiter.api.Assertions.*;
-import static sample.cafekiosk.spring.domain.order.OrderStatus.PAYMENT_COMPLETED;
+import static sample.cafekiosk.spring.domain.order.OrderStatus.*;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
@@ -27,6 +29,8 @@ class OrderRepositoryTest extends IntegrationTestSupport {
 
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @DisplayName("하루치 결제 완료된 주문목록을 조회한다.")
     @Test
@@ -36,10 +40,14 @@ class OrderRepositoryTest extends IntegrationTestSupport {
         LocalDateTime startDateTime = LocalDate.now().atStartOfDay();
         LocalDateTime endDateTime = LocalDate.now().plusDays(1).atStartOfDay();
 
-        List<Product> products1 = List.of(createProduct("001", "아메리카노",1000));
-        List<Product> products2 = List.of(createProduct("002", "카페라떼",3000));
-        Order order1 = Order.create(products1, registeredDateTime);
-        Order order2 = Order.create(products2, endDateTime);
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        List<Product> products = List.of(product1, product2, product3);
+        productRepository.saveAll(products);
+
+        Order order1 = create(products, registeredDateTime, PAYMENT_COMPLETED);
+        Order order2 = create(products, endDateTime, PAYMENT_FAILED);
         orderRepository.saveAll(List.of(order1,order2));
 
         // when
@@ -54,13 +62,21 @@ class OrderRepositoryTest extends IntegrationTestSupport {
                 ); // 순서 상관없이 맞는지 체크
     }
 
-    private Product createProduct(String productNumber, String name, int price) {
+    private Product createProduct(ProductType type, String productNumber, int price) {
         return Product.builder()
-                .type(HANDMADE)
+                .type(type)
                 .productNumber(productNumber)
                 .price(price)
                 .sellingStatus(SELLING)
-                .name(name)
+                .name("메뉴 이름")
+                .build();
+    }
+
+    public Order create(List<Product> products, LocalDateTime registeredDatetime, OrderStatus status) {
+        return Order.builder()
+                .orderStatus(status)
+                .products(products)
+                .registeredDateTime(registeredDatetime)
                 .build();
     }
 
